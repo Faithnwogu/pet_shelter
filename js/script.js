f0126b45dd330825b30577a06d19a0087a96c17b
Faithnwogu <dfaytech@gmail.com>
2023-09-25 12:20:03 +0100

    code for pet shelter

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// navigation
+const mobileIcon = document.getElementById('mobileIcon');
+const mobileLinks = document.getElementById('mobileLinks');
+
+mobileIcon.addEventListener('click', ()=>{
+        mobileLinks.classList.toggle('toggle');
+        
+})
